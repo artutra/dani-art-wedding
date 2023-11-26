68e87282aa8d83c76ed7f2ce0368dd49ce32f9bd
Daniel Pantu <danielpantu.code@gmail.com>
2023-11-25 22:20:22 -0300

    Add custom fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./layouts/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        italiana: ["Italiana", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"]
+      },
+      colors: {
+        'moss': '#577453',
+        'terracotta': '#B02C0C',
+        'primary': '#FCF7FF',
+        'secondary': '#878C8F',
+      }
+    },
   },
   plugins: [],
 }
